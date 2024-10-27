d63bdbffb7e4b29b35d8e0769da8f0518ea7081a
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-10-27 03:49:57 +0200

    Finshed Javascript

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-for(let i=1; i<=5; i++) {
-    for (let j = 1; j<= i; j++){
-    process.stdout.write("*");
-    }
-    console.log()
-}
+const number = parseInt(prompt("Enter a number: "));
 
+for (let i = 1; i <= number; i++) {
+    for (let j = 1; j <= i; j++) {
+        process.stdout.write(j.toString());  
+    }
+    console.log();  
+}
