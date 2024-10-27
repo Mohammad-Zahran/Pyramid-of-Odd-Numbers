790a5f1c543e6c97e5b2d9fe75fce7eb5900595c
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-10-27 22:29:55 +0200

    Fixed the problems in index.js

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const number = parseInt(prompt("Enter a number: "));
 
+
 for (let i = 1; i <= number; i++) {
+    let row = ""; 
     for (let j = 1; j <= i; j++) {
-        process.stdout.write(j.toString());  
+       row += (2 * j - 1).toString(); 
     }
-    console.log();  
+    console.log(row); 
 }
+
+/* I used this formula to multiply by two to garantee getting
+even number after getting the even number I subtract by 1 
+to have an odd number I used this method also in python
+*/
--- index.py
+++ index.py
@@ -1,6 +1,6 @@
-number = int(input("Enter the number of rows: "))  
+number = int(input("Enter the number of rows: "))
 
 for i in range(1, number + 1):
-    for j in range(1, i + 1): 
-        print(j, end="") 
-    print("")
+    for j in range(1, i + 1):
+        print(2 * j - 1, end="")  
+    print("")  
